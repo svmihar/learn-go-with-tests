5ddb55290cd2305adb94225debd3d40186454b0b
svmihar <svmihar@gmail.com>
2019-12-10 23:41:45 +0700

    refactoring test on asserting errors

--- src/1_hello/hello.go
+++ src/1_hello/hello.go
@@ -3,12 +3,28 @@ package main
 import "fmt"
 
 const sapaan = "Hello, "
+const sapaanIndo = "Hallo, "
+const sapaanFrench = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, lang string) string {
 	if name == "" {
 		name = "World"
 	}
-	return sapaan + name + "!"
+	return sapaanPrefix(lang) + name
+}
+
+func sapaanPrefix(lang string) (prefix string) {
+	switch lang {
+	case "French":
+		prefix = sapaanFrench
+	case "Indo":
+		prefix = sapaanIndo
+	default:
+		prefix = sapaan
+	}
+	return
+	// amazing, dengan bikin (prefix string) buat return value-nya gak perlu di tulis lagi di returnya karena otomatis ngebuat variable 'prefix'
+
 }
 
 func main() {
--- src/1_hello/hello_test.go
+++ src/1_hello/hello_test.go
@@ -3,20 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people's name", func(t *testing.T) {
-		got := Hello("Tian")
-		want := "Hello, Tian!"
-
+	singNggenaKoen := func(t *testing.T, got, want string) {
+		t.Helper() // supaya kalo ada error ditunjukkin line number yang sesuai, bukan di refer ke method ini
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("saying hello to people's name", func(t *testing.T) {
+		got := Hello("Tian")
+		want := "Hello, Tian!"
+		singNggenaKoen(t, got, want)
 	})
 	// failing test
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		singNggenaKoen(t, got, want)
+	})
+
+	t.Run("say hello in indonesian", func(t *testing.T) {
+		got := Hello("Tian", "Indo")
+		want := "Halo, Tian"
+		singNggenaKoen(t, got, want)
 	})
 }
