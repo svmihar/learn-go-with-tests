3d927a88a4024a6f8b93fe1fa2e2b98cd363dc94
svmihar <svmihar@gmail.com>
2019-12-10 20:03:41 +0700

    added new set of subtest and helper

--- src/1_hello/hello.go
+++ src/1_hello/hello.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, World!"
+const sapaan = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return sapaan + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Tian"))
 }
--- src/1_hello/hello_test.go
+++ src/1_hello/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World!"
+	t.Run("saying hello to people's name", func(t *testing.T) {
+		got := Hello("Tian")
+		want := "Hello, Tian!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	// failing test
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
