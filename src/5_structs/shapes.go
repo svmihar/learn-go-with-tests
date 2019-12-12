b5e4e4a2562fbdc3f4ada6efbf4d53c2d2451a0d
svmihar <svmihar@gmail.com>
2019-12-13 03:35:10 +0700

    parametric polymorphism UwU

--- src/5_structs/shapes.go
+++ src/5_structs/shapes.go
@@ -1,9 +1,32 @@
 package main
 
+import "math"
+
 func Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
 }
 
 func Area(width float64, height float64) float64 {
 	return width * height
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
 }
--- src/5_structs/shapes_test.go
+++ src/5_structs/shapes_test.go
@@ -3,7 +3,9 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+
 	want := 40.0
 
 	if got != want {
@@ -12,10 +14,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
